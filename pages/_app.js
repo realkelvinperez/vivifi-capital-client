453515e84b56213e57773196fb8c521f67a8e281
realkelvinperez <realkelvinperez@gmail.com>
2021-04-26 16:39:38 -0400

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react"
+const theme = extendTheme({ initialColorMode: "dark" })
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+  )
 }
-
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,49 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { Global, css } from "@emotion/react";
+
+import {
+    Button,
+    Container,
+    Flex,
+    Heading
+} from "@chakra-ui/react";
 
 export default function Home() {
+
+	const handleClick = () => {
+		console.log({msg: 'Button Clicked'})
+	}
+
   return (
-    <div className={styles.container}>
+    <div>
+        <Global
+            styles={
+                css`
+                     body, #__next, #__next div {
+                       min-height: 100vh;
+                       min-height: -webkit-fill-available;
+                     }
+                `
+            }
+        />
       <Head>
-        <title>Create Next App</title>
+        <title>ViViFi Capital</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
+      <Container as="main">
+          <Flex
+              direction={'column'}
+              alignItems={'center'}
+              justifyContent={'center'}
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+              <Heading marginY={1}>ViViFi Capital</Heading>
+              <Heading as="h3" size="sm">Dashboard</Heading>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+              <Button marginY={4} onClick={handleClick}>
+                  Connect Wallet
+              </Button>
+          </Flex>
+      </Container>
     </div>
   )
 }
